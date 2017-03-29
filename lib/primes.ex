0ab455594640f8958b8965f720bd642a0b9338c0
James Long <jamesharwoodlong@gmail.com>
2017-03-29 15:49:44 +0100

    Add performance testing code

--- lib/primes.ex
+++ lib/primes.ex
@@ -21,4 +21,22 @@ defmodule Primes do
     |> Enum.to_list()
     |> (fn list -> [2 | list] end).()
   end
+
+  def compare_solutions(n) do
+    to_test = [{"Brute force", &first_n_primes/1}]
+
+    IO.puts "N = #{n}"
+    Enum.each(to_test, fn {name, function} ->
+      duration = time(fn -> function.(n) end)
+      IO.inspect "#{name}: #{duration}s"
+    end)
+    IO.puts "Done"
+  end
+
+  def time(function) do
+    function
+    |> :timer.tc
+    |> elem(0)
+    |> Kernel./(1_000_000)
+  end
 end
