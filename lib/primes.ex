56f9f58aedf6049008675f994ed0c710f42633ff
James Long <jamesharwoodlong@gmail.com>
2017-03-29 13:21:53 +0100

    Add prime? function (to be used in tests)
    NB. I had already implemented prime?/1 for an exercism exercise. See:
    https://gitlab.com/jameslong/exercism/blob/master/elixir/nth-prime/nth_prime.exs

--- lib/primes.ex
+++ lib/primes.ex
@@ -3,5 +3,13 @@ defmodule Primes do
   Functions related to prime number generation.
   """
 
+  def prime?(1) do false end
+  def prime?(2) do true end
+  def prime?(x) do
+    upper_bound = round(:math.sqrt(x))
+    2..upper_bound
+    |> Enum.all?(&(rem(x, &1) != 0))
+  end
+
   def first_n_primes(_n), do: []
 end
--- test/primes_test.exs
+++ test/primes_test.exs
@@ -2,11 +2,30 @@ defmodule PrimesTest do
   use ExUnit.Case
   doctest Primes
 
+  @first_10_primes [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+  @first_10_nonprimes [1, 4, 6, 8, 9, 10, 12, 14, 15, 16]
+
+  test "primality of 1" do
+    refute Primes.prime?(1)
+  end
+
+  test "primality of 2" do
+    assert Primes.prime?(2)
+  end
+
+  test "primality of first 10 primes" do
+    assert Enum.all?(@first_10_primes, &Primes.prime?/1)
+  end
+
+  test "primality of first 10 non-primes" do
+    refute Enum.any?(@first_10_nonprimes, &Primes.prime?/1)
+  end
+
   test "first prime" do
     assert Primes.first_n_primes(1) == [2]
   end
 
   test "first 10 primes" do
-    assert Primes.first_n_primes(10) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+    assert Primes.first_n_primes(10) == @first_10_primes
   end
 end
