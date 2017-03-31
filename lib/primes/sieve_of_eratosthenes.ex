b2c76b1258024ba7db2d74185f7466247eed6929
James Long <jamesharwoodlong@gmail.com>
2017-03-31 19:00:43 +0100

    Add docs and refactor sieve solution for greater clarity

--- lib/primes/sieve_of_eratosthenes.ex
+++ lib/primes/sieve_of_eratosthenes.ex
@@ -3,32 +3,51 @@ defmodule Primes.SieveOfEratosthenes do
   Lazy Sieve of Eratosthenes prime number generator.
   """
 
+  @doc """
+  Generates the first n primes using a lazy Sieve of Eratosthenes.
+
+  An accumulator 'multiples' is used to store the next multiple of each prime
+  found as well as the prime factors of that multiple. A number is prime if,
+  when reached, it does not exist in 'multiples'.
+
+  As optimisations we don't store even multiples of the primes, and only store
+  from the square of the prime and above.
+
+  For a more in depth explanation of this algorithm please see:
+  http://www.cs.hmc.edu/~oneill/papers/Sieve-JFP.pdf
+  """
+
   def first_n_primes(1), do: [2]
   def first_n_primes(2), do: [2, 3]
   def first_n_primes(n) do
     3
     |> Stream.iterate(&(&1 + 2))
-    |> Stream.transform(%{}, fn (i, acc) ->
-      case Map.get(acc, i) do
-        nil -> # prime
-          square = i * i
-          updated_acc = add_to_key_value(acc, square, i)
-          {[i], updated_acc}
-        factors -> # not prime
-          removed = Map.delete(acc, i)
-          updated_acc = Enum.reduce(factors, removed, fn (fact, acc) ->
-            add_to_key_value(acc, i + (2 * fact), fact)
-          end)
-          {[], updated_acc}
+    |> Stream.transform(%{}, fn (i, multiples) ->
+      case Map.get(multiples, i) do
+        nil -> # i is prime
+          new_multiples = store_multiple(multiples, i * i, i)
+          {[i], new_multiples}
+        factors -> # i is not prime
+          new_multiples =
+            multiples
+            |> Map.delete(i)
+            |> store_next_multiples(i, factors)
+          {[], new_multiples}
       end
     end)
     |> Enum.take(n - 1)
     |> List.insert_at(0, 2)
   end
 
-  defp add_to_key_value(map, key, value) do
-    values = Map.get(map, key, [])
-    new_values = [value | values]
-    Map.put(map, key, new_values)
+  defp store_next_multiples(multiples, i, factors) do
+    Enum.reduce(factors, multiples, fn (fact, acc) ->
+     store_multiple(acc, i + (2 * fact), fact)
+    end)
+  end
+
+  defp store_multiple(multiples, i, factor) do
+    factors = Map.get(multiples, i, [])
+    new_factors = [factor | factors]
+    Map.put(multiples, i, new_factors)
   end
 end
